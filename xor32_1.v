d8020eae2383c7b7660b5d4645eff6410f58913c
Freekick1704 <jichen.zhang@outlook.com>
2021-09-13 07:18:19 +0800

    Fixed errors in xor32_1

--- xor32_1.v
+++ xor32_1.v
@@ -1,11 +1,12 @@
 module xor32_1(a, b, out);
 	input [31:0] a;
-	intput b;
+	input b;
 	output [31:0] out;
-	genvar i;
 	generate
-		for (i = 0; i < 32; i = i + 1) begin
-			xor xor_gate(a[i], b, out[i]);
-		end
+	genvar i;
+		for (i = 0; i < 32; i = i + 1)
+			begin : gen1
+				xor xor_gate(out[i], a[i], b);
+			end
 	endgenerate
 endmodule
