5a19c077f775d40d4e3876d66389537a012a07bb
zianwangs <Wzan980311@163.com>
2021-09-13 14:12:21 -0400

    fix variable name, the code could even compile successfully before fixing!

--- alu.v
+++ alu.v
@@ -10,7 +10,7 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 	wire ci, co;
 	wire [31:0] data_operandB_adjusted;
 	assign ci = ctrl_ALUopcode[0];
-	xor32_1 adjustB(data_operandB, ci, data_operandB_adjuested);
+	xor32_1 adjustB(data_operandB, ci, data_operandB_adjusted);
 	csa_32 compute(data_operandA, data_operandB_adjusted, ci, co, overflow, data_result);
 
 
